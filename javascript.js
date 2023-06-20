7ff65b6267dc35bf15db13dff7cfa324869e6299
godwinmurithi <godiewyn54@gmail.com>
2023-06-20 13:36:36 +0300

    Changed Variable names

--- javascript.js
+++ javascript.js
@@ -5,6 +5,28 @@ let message;
 // copy 'Hello world' from hello into message
 message = hello;
 
-// now two variables hold the same data
-alert(hello); // Hello world!
-alert(message); // Hello world!
+//alert(hello); // Hello world!
+//alert(message); // Hello world!
+
+//Constants
+
+const myBirthDay = '18.01.2023';
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+//alert(color); // #FF7F00
+
+let admin, name; // can declare two variables at once
+
+name = "John";
+
+admin = name;
+
+alert( admin ); // "John"
+
+let ourPlanetName = 'Planet Earth';
+let currentUserName = 'John'
